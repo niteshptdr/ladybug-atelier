659f87cf167ea611b4fb56fabeff805b92506739
niteshptdr <niteshpatidar88@gmail.com>
2025-09-01 15:47:15 +0530

    url

--- boutique/urls.py
+++ boutique/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', include('ladybug.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
